60e78ca5523d098f12676b9f89edbb860055d42e
Matheus Crivellari <matheuscrivellari@pecege.com>
2019-07-15 16:04:52 -0300

    feat: validate all form fields

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Main extends Component {
+    state = {
+        email: {
+            value: '',
+            invalid: false,
+            valid: false,
+        },
+
+        password: {
+            value: '',
+            invalid: false,
+            valid: false,
+        },
+
+        recoverPassword: {
+            value: '',
+            invalid: false,
+            valid: false,
+        },
+    };
+
     /**
      * Callback invoked when
      * login form is submitted
      */
-    handleEnter = () => {
+    handleEnter = e => {
+        e.preventDefault();
         console.log('Enter the website');
     }
 
@@ -14,7 +35,8 @@ export default class Main extends Component {
      * Callback invoked when Password
      * recovery form is submitted
      */
-    handleRecoverPasswd = () => {
+    handleRecoverPasswd = e => {
+        e.preventDefault();
         console.log('Recover your password');
     }
 
@@ -23,6 +45,97 @@ export default class Main extends Component {
      */
     updateTitle() {
         document.title = !this.recover ? 'Login' : 'Password recovery';
+    }
+
+    /**
+     * Event fired when typing in
+     * login form email input
+     */
+    handleUpdateEmail = e => {
+        const email = e.target.value;
+
+        this.setState({
+            ...this.state,
+            email: {
+                value: email,
+                invalid: this.validateEmail(email),
+                valid: !this.validateEmail(email),
+            },
+        });
+    }
+
+    /**
+     * Event fired when typing in
+     * login form password input
+     */
+    handleUpdatePassword = e => {
+        const password = e.target.value;
+
+        this.setState({
+            ...this.state,
+            password: {
+                value: password,
+                invalid: this.validatePassword(password),
+                valid: !this.validatePassword(password),
+            },
+        });
+    }
+
+    /**
+     * Event fired when typing in password
+     * recoery form email input
+     */
+    handleUpdateRecoverEmail = e => {
+        const email = e.target.value;
+
+        this.setState({
+            ...this.state,
+            recoverPassword: {
+                value: email,
+                invalid: this.validateEmail(email),
+                valid: !this.validateEmail(email),
+            },
+        });
+    }
+
+    /**
+     * Checks if email is a valid email address
+     * @param {string} email Email string
+     */
+    validateEmail(email) {
+
+        const validEmail = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+            .test(email);
+
+        let invalid = false;
+
+        if(email === '')
+            invalid = true;
+
+        if(!validEmail)
+            invalid = true;
+
+        if(email.length < 10)
+            invalid = true;
+
+        return invalid;
+    }
+
+    /**
+     * Checks if password is valid
+     * @param {string} pass Password string
+     */
+    validatePassword(pass) {
+
+        let invalid = false;
+
+        if(pass === '')
+            invalid = true;
+
+        if(pass.length < 6)
+            invalid = true;
+
+        return invalid;
     }
 
     /**
@@ -49,6 +162,14 @@ export default class Main extends Component {
         return this.props.match.params.passwd === 'password-recover';
     }
 
+    get loginFormValid() {
+        return this.state.email.invalid || this.state.email.value === '' || this.state.password.invalid || this.state.password.value === '';
+    }
+
+    get passRecoverFormValid() {
+        return this.state.recoverPassword.invalid || this.state.recoverPassword.value === '';
+    }
+
     /**
      * Renders actual page
      */
@@ -57,11 +178,11 @@ export default class Main extends Component {
             <div className="container-fluid py-5">
                 <div className="row">
                     <div className="col-10 offset-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 p-0 justify-content-center">
-                        <div id="login-wrapper" className={ 'bg-light shadow-lg mt-5 bg-success' + (this.recover ? ' recover' : null) }>
+                        <div id="login-wrapper" className={ 'bg-light shadow-lg mt-5 bg-success' + (this.recover ? ' recover' : '') }>
                             <div className="form-wrapper">
                                 <div className="wrapper">
                                     <div className="login">
-                                        <form onSubmit={this.handleEnter}>
+                                        <form noValidate={true} onSubmit={this.handleEnter}>
                                             <h2 className="mb-2">Enter</h2>
                                             <br/>
                                             <div className="form-group mb-3">
@@ -70,7 +191,9 @@ export default class Main extends Component {
                                                     type="text"
                                                     placeholder="e-mail"
                                                     id="email"
-                                                    className="form-control p-4 login-email"
+                                                    className={ 'form-control p-4 login-email' + (this.state.email.invalid ? ' is-invalid' : '') + (this.state.email.valid ? ' is-valid' : '')}
+                                                    onChange={this.handleUpdateEmail}
+                                                    value={this.state.email.value}
                                                     autoComplete="off"
                                                     />
                                                     <small className="text-left invalid-feedback">
@@ -83,7 +206,9 @@ export default class Main extends Component {
                                                     type="password"
                                                     placeholder="password"
                                                     id="password"
-                                                    className="form-control p-4 login-password"
+                                                    className={ 'form-control p-4 login-password' + (this.state.password.invalid ? ' is-invalid' : '') + (this.state.password.valid ? ' is-valid' : '')}
+                                                    onChange={this.handleUpdatePassword}
+                                                    value={this.state.password.value}
                                                     autoComplete="off"
                                                     />
                                                     <small className="text-left invalid-feedback">
@@ -96,7 +221,10 @@ export default class Main extends Component {
                                                 </Link>
                                             </p>
                                             <br/>
-                                            <button className="btn btn-lg mb-5">
+                                            <button
+                                                disabled={this.loginFormValid}
+                                                className="btn btn-lg mb-5"
+                                                >
                                                 Enter
                                             </button>
                                             <br/>
@@ -114,7 +242,7 @@ export default class Main extends Component {
                                 </div>
                                 <div className="wrapper">
                                     <div className="recover">
-                                        <form onSubmit={this.handleRecoverPasswd}>
+                                        <form noValidate={true} onSubmit={this.handleRecoverPasswd}>
                                             <h2 className="mb-3">Forgot password</h2>
                                             <br/>
                                             <p className="mb-3">
@@ -128,7 +256,8 @@ export default class Main extends Component {
                                                     type="text"
                                                     placeholder="e-mail"
                                                     id="recoverEmail"
-                                                    className="form-control p-4 login-email"
+                                                    className={ 'form-control p-4 login-email' + (this.state.recoverPassword.invalid ? ' is-invalid' : '') + (this.state.recoverPassword.valid ? ' is-valid' : '')}
+                                                    onChange={this.handleUpdateRecoverEmail}
                                                     autoComplete="off"
                                                     />
                                                     <small className="text-left invalid-feedback">
@@ -140,7 +269,10 @@ export default class Main extends Component {
                                                 <Link to="/"> Enter</Link>
                                             </p>
                                             <br/>
-                                            <button className="btn btn-lg my-5">
+                                            <button
+                                                disabled={this.passRecoverFormValid}
+                                                className="btn btn-lg my-5"
+                                                >
                                                 Recover password
                                             </button>
                                         </form>
